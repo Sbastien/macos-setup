2bf57c4eab0e10a46be7429561e3eb718d3226bc
Sébastien Loyer <loyer.sebastien@gmail.com>
2020-02-22 12:53:42 +0100

    Update iTerm shell integration

--- tools.sh
+++ tools.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install shell integration for iTerm2
-curl -L https://iterm2.com/misc/`basename $SHELL`_startup.in -o ~/.iterm2_shell_integration.`basename $SHELL`
-source ~/.iterm2_shell_integration.`basename $SHELL`
+curl -L https://iterm2.com/shell_integration/`basename $SHELL` \
+     -o ~/.iterm2_shell_integration.`basename $SHELL`
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
